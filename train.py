5858dcdcf0812aafc0fcc8ab79416ea50f64ab29
mooofna <105771470+mooofna@users.noreply.github.com>
2024-04-03 15:26:14 +0200

    Add train loop

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataSet, DataLoader, random_split
+from torch.utils.data import Dataset, DataLoader, random_split
+
+from dataset import BilingualDataset, causal_mask
+from model import Transformer, build_transformer
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
@@ -37,3 +40,38 @@ def get_dataset(config):
     train_ds_size = int(len(ds_raw) * 0.9)
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+
+    max_len_src = 0
+    max_len_tgt = 0
+
+    for item in ds_raw:
+        src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+        tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+        max_len_src = max(max_len_src, len(src_ids))
+        max_len_tgt = max(max_len_tgt, len(tgt_ids))
+
+    print(f'Maximum length of source language: {max_len_src}')
+    print(f'Maximum length of target language: {max_len_tgt}')
+
+    train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
+    val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
+
+    return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+def get_model(config, vocab_src_len, vocab_tgt_len):
+    model = build_transformer(vocab_src_len, vocab_tgt_len, config['seq_len'], config['seq_len'], config['d_model'])
+    return model
+
+def train_model(config):
+    # Define the device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'Device: {device}')
+
+    Path(config['model_folder']).mkdir(parents=True, exist_ok=True)
+
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_dataset(config)
+
+    
